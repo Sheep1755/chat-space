d5acb8dcf2811c961e71ade0601803cc74df26c9
sheep1755 <the_straw_hat@icloud.com>
2020-07-09 15:54:01 +0900

    fix2-chatspace

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -42,10 +42,10 @@ $(function(){
 
   let reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    let last_message_id = $('.message-group:last').data("message-id");
+    let last_message_id = $('.message-group:last').data("message-id")||0;
     $.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      url:  "api/messages",
+      url: "api/messages",
       //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
